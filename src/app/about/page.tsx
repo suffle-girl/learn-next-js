46a8b36ffaef4127d311960ce296fad320b48000
Šárka F. <sarka.flidrova@email.cz>
2025-01-17 14:54:20 +0100

    Routing.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <h1>About me</h1>;
+};
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,3 +3,9 @@ const Home = () => {
 };
 
 export default Home;
+
+// Routing
+// URL paths that usesrs can access in the browser are defined by files and folders in my code base
+// needs a folder called app and within it a page.tsx or page.js file with exported component
+// layout.tsx gets created automatically
+// Next automatically handles a non-existing route with its own 404 page
